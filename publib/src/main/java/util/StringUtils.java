d804e73a5a53c13c068604ba5741492546eed11e
jwg <sky_jiangwg@163.com>
2019-03-07 11:19:03 +0800

    string工具类增加去空格

--- publib/src/main/java/util/StringUtils.java
+++ publib/src/main/java/util/StringUtils.java
@@ -5,4 +5,13 @@ public class StringUtils {
     public static boolean isEmpty(String str){
         return str == null || str.trim().length() == 0 ;
     }
+
+    public static String trim(String text) {
+        if(text == null || "".equals(text)) {
+            return text;
+        }
+
+        return text.trim();
+    }
+
 }
