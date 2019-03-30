8f42b66f2fa93cef7abc9d5a37d2218f5cc795b8
skyjiangwg <sky_jiangwg@163.com>
2019-03-30 14:23:03 +0800

    全局字典 蒋文广

---
+++ application-global/src/main/java/com/onek/util/dict/DictStore.java
@@ -0,0 +1,89 @@
+package com.onek.util.dict;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.onek.consts.CSTATUS;
+import redis.IRedisCache;
+import redis.annation.CacheField;
+import redis.proxy.CacheProxyInstance;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+public class DictStore{
+
+    private static IRedisCache dictProxy =(IRedisCache) CacheProxyInstance.createInstance(new DictUtil());
+
+    public  static JSONObject getAllDict(){
+        JSONObject j = new JSONObject();
+        List<DictEntity> dictList = (List<DictEntity>)dictProxy.queryAll();
+        if(dictList != null && dictList.size() > 0){
+            Map<String, List<DictEntity>> dictMap = new HashMap<>();
+            for(DictEntity dictVo : dictList){
+                List<DictEntity> subList = dictMap.get(dictVo.getType());
+                if(subList == null) subList = new ArrayList<>();
+                if((dictVo.getCstatus() & CSTATUS.DELETE) > 0){
+                    continue;
+                }
+                subList.add(dictVo);
+                dictMap.put(dictVo.getType(), subList);
+            }
+
+            for (String type : dictMap.keySet()){
+                JSONArray dictArr = new JSONArray();
+                List<DictEntity> subList = dictMap.get(type);
+                for(DictEntity dictVo : subList){
+                    JSONObject obj = new JSONObject();
+                    obj.put("value", dictVo.getDictc());
+                    obj.put("text", dictVo.getText());
+                    obj.put("label", dictVo.getText());
+                    obj.put("remark", dictVo.getText());
+                    obj.put("customc", dictVo.getCustomc());
+                    dictArr.add(obj);
+                }
+                j.put(type, dictArr);
+            }
+        }
+        return j;
+    }
+
+    public static List<?> translate(List<?> list) throws Exception{
+        if(list != null && list.size() > 0) {
+            Field[] fields = list.get(0).getClass().getDeclaredFields();
+            Map<Field, CacheField> cacheFieldMap = new HashMap<>();
+            for(Field f : fields) {
+                CacheField cache = f.getDeclaredAnnotation(CacheField.class);
+                if(cache != null) {
+                    cacheFieldMap.put(f, cache);
+                }
+            }
+            Set<Field> fieldKeys = cacheFieldMap.keySet();
+            for(Field field : fieldKeys) {
+                CacheField cacheField = cacheFieldMap.get(field);
+                String col = cacheField.cachecolumn();
+                String refcol = cacheField.reflectcolumn();
+                String key = cacheField.key();
+                String prefix = cacheField.prefix();
+
+
+                for (Object object : list) {
+                    field.setAccessible(true);
+                    Object id = field.get(object);
+                    Field f = object.getClass().getDeclaredField(refcol);
+                    f.setAccessible(true);
+                    if(prefix.equals("dict_")) {
+                        DictEntity dictVo = (DictEntity)dictProxy.getId(id);
+                        Field ff = dictVo.getClass().getDeclaredField(col);
+                        ff.setAccessible(true);
+                        Object value = ff.get(dictVo);
+                        f.set(object, value);
+                    }
+                }
+            }
+
+        }
+
+        return list;
+    }
+
+}
