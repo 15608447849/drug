d293f4d129976b33a08fbef04218d6241551478c
skyjiangwg <sky_jiangwg@163.com>
2019-04-04 16:51:19 +0800

    蒋文广 20190404

--- server-goods/src/main/java/com/onek/goods/ProdModule.java
+++ server-goods/src/main/java/com/onek/goods/ProdModule.java
@@ -15,6 +15,7 @@ import com.onek.goods.entities.MallFloorVO;
 import com.onek.goods.entities.ProdVO;
 import com.onek.goods.service.MallFloorImpl;
 import com.onek.goods.util.ProdESUtil;
+import com.onek.util.dict.DictStore;
 import com.onek.util.dict.DictUtil;
 import com.onek.util.prod.ProdEntity;
 import objectref.ObjectRefUtil;
@@ -30,7 +31,7 @@ import static com.onek.goods.util.ProdESUtil.searchProd;
 
 public class ProdModule {
 
-    private static IRedisCache mallFloorProxy =(IRedisCache) CacheProxyInstance.createInstance(new MallFloorImpl());
+    private static IRedisCache mallFloorProxy = (IRedisCache) CacheProxyInstance.createInstance(new MallFloorImpl());
 
     public static final Long NEW = 1L;
     public static final Long HOT = 2L;
@@ -38,50 +39,57 @@ public class ProdModule {
 
     @SuppressWarnings("unchecked")
     @UserPermission(ignore = true)
-    public Result getMallFloorProd(AppContext appContext){
+    public Result getMallFloorProd(AppContext appContext) {
         List<MallFloorVO> mallFloorVOList = (List<MallFloorVO>) mallFloorProxy.queryAll();
         Result r = new Result();
         JSONObject jsonObject = new JSONObject();
-        if(mallFloorVOList != null && mallFloorVOList.size() > 0){
+        if (mallFloorVOList != null && mallFloorVOList.size() > 0) {
             List<ProdVO> prodVOList = new ArrayList<>();
             SearchResponse response = ProdESUtil.searchProdWithMallFloor("128", 1, 100);
-            if(response == null || response.getHits().totalHits <= 0){
+            if (response == null || response.getHits().totalHits <= 0) {
                 response = ProdESUtil.searchProdWithMallFloor("", 1, 100);
             }
             List<ProdVO> prodList = new ArrayList<>();
-            for(SearchHit searchHit : response.getHits()){
+            for (SearchHit searchHit : response.getHits()) {
                 ProdVO prodVO = new ProdVO();
                 Map<String, Object> sourceMap = searchHit.getSourceAsMap();
 
-                HashMap detail = (HashMap)sourceMap.get("detail");
+                HashMap detail = (HashMap) sourceMap.get("detail");
                 BeanMapUtils.mapToBean(detail, prodVO);
 
                 prodList.add(prodVO);
+                try{
+                    DictStore.translate(prodVO);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
             }
-             for(MallFloorVO floorVO : mallFloorVOList){
-                 if(floorVO.getUnqid() == NEW){
-                     List<ProdVO> filterProdList = loadProd(prodList, 256);
-                     jsonObject.put("new", filterProdList);
-                 }else if(floorVO.getUnqid() == HOT){
-                     List<ProdVO> filterProdList = loadProd(prodList, 128);
-                     jsonObject.put("hot", filterProdList);
-                 }
-             }
+
+
+            for (MallFloorVO floorVO : mallFloorVOList) {
+                if (floorVO.getUnqid() == NEW) {
+                    List<ProdVO> filterProdList = loadProd(prodList, 256);
+                    jsonObject.put("new", filterProdList);
+                } else if (floorVO.getUnqid() == HOT) {
+                    List<ProdVO> filterProdList = loadProd(prodList, 128);
+                    jsonObject.put("hot", filterProdList);
+                }
+            }
         }
         return new Result().success(jsonObject);
     }
 
-    private List<ProdVO> loadProd(List<ProdVO> prodList,int type) {
+    private List<ProdVO> loadProd(List<ProdVO> prodList, int type) {
         List<ProdVO> newProdList = new ArrayList<>();
-        for(ProdVO prodVO : prodList){
-            if((prodVO.getCstatus() & 256) > 0){
+        for (ProdVO prodVO : prodList) {
+            if ((prodVO.getCstatus() & 256) > 0) {
                 newProdList.add(prodVO);
             }
         }
-        if(newProdList.size()<=0){
+        if (newProdList.size() <= 0) {
             int i = 10;
-            for(ProdVO prodVO : prodList){
-                if(i++>10){
+            for (ProdVO prodVO : prodList) {
+                if (i++ > 10) {
                     break;
                 }
                 newProdList.add(prodVO);
@@ -96,10 +104,10 @@ public class ProdModule {
         String keyword = json.get("keyword").getAsString();
         JsonArray specArray = json.get("specArray").getAsJsonArray();
         JsonArray manuArray = json.get("manuArray").getAsJsonArray();
-        List<String> specList =new ArrayList<>();
-        if(specArray != null && specArray.size() > 0){
+        List<String> specList = new ArrayList<>();
+        if (specArray != null && specArray.size() > 0) {
             Iterator<JsonElement> it = specArray.iterator();
-            while(it.hasNext()){
+            while (it.hasNext()) {
                 JsonElement elem = it.next();
                 specList.add(elem.getAsJsonObject().get("val").getAsString());
             }
@@ -109,10 +117,10 @@ public class ProdModule {
 //           System.out.println("spec:"+spec);
 //       }
 
-        List<Long> manunoList =new ArrayList<>();
-        if(manuArray != null && manuArray.size() > 0){
+        List<Long> manunoList = new ArrayList<>();
+        if (manuArray != null && manuArray.size() > 0) {
             Iterator<JsonElement> it = manuArray.iterator();
-            while(it.hasNext()){
+            while (it.hasNext()) {
                 JsonElement elem = it.next();
                 manunoList.add(elem.getAsJsonObject().get("val").getAsLong());
             }
@@ -120,10 +128,10 @@ public class ProdModule {
 
         Result r = new Result();
         SearchResponse response = ProdESUtil.searchProd(keyword, specList, manunoList, appContext.param.pageIndex, appContext.param.pageNumber);
-        List<Map<String,Object>> resultList = new ArrayList<>();
-        if(response != null){
+        List<Map<String, Object>> resultList = new ArrayList<>();
+        if (response != null) {
             for (SearchHit searchHit : response.getHits()) {
-                Map<String,Object> sourceMap = searchHit.getSourceAsMap();
+                Map<String, Object> sourceMap = searchHit.getSourceAsMap();
                 resultList.add(sourceMap);
             }
 
@@ -132,7 +140,7 @@ public class ProdModule {
         Page page = new Page();
         page.pageSize = appContext.param.pageNumber;
         page.pageIndex = appContext.param.pageIndex;
-        page.totalItems = response!=null && response.getHits() != null ? (int)response.getHits().totalHits : 0;
+        page.totalItems = response != null && response.getHits() != null ? (int) response.getHits().totalHits : 0;
         PageHolder pageHolder = new PageHolder(page);
         pageHolder.value = page;
         return r.setQuery(resultList, pageHolder);
