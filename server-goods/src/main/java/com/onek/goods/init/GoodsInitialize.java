9372e4ba2bfb84f1eb96f0974313fc197ff71b8d
skyjiangwg <sky_jiangwg@163.com>
2019-04-12 15:21:05 +0800

    redis优化配置 蒋文广 20190412

--- server-goods/src/main/java/com/onek/goods/init/GoodsInitialize.java
+++ server-goods/src/main/java/com/onek/goods/init/GoodsInitialize.java
@@ -2,11 +2,11 @@ package com.onek.goods.init;
 
 import com.onek.goods.timer.HigherProfitsTimer;
 import com.onek.goods.timer.RemoveNewFlagTimer;
-import com.onek.server.infimp.IIceIceInitialize;
+import com.onek.server.infimp.IIceInitialize;
 
 import static Ice.Application.communicator;
 
-public class GoodsInitialize implements IIceIceInitialize {
+public class GoodsInitialize implements IIceInitialize {
     @Override
     public void startUp(String serverName) {
         communicator().getLogger().error(serverName+" 开始初始化 --- ");
