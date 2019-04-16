1b04ba0f3e3cf35f1729f56e2db40cfd4f0ee056
skyjiangwg <sky_jiangwg@163.com>
2019-04-16 20:36:54 +0800

    蒋文广 会员模块 20190416

---
+++ server-user/src/main/java/com/onek/user/MemberModule.java
@@ -0,0 +1,11 @@
+package com.onek.user;
+
+import com.onek.context.AppContext;
+import com.onek.entitys.Result;
+
+public class MemberModule {
+
+    public Result updateMember(AppContext appContext) {
+
+    }
+}
