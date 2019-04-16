9094d0ea20bc15efa2eebcf24694cdd655c53335
liuhuics <liuhuics@126.com>
2019-04-16 17:23:42 +0800

    添加订单详情表

--- jdbc/src/main/java/constant/DSMConst.java
+++ jdbc/src/main/java/constant/DSMConst.java
@@ -53,7 +53,8 @@ public interface DSMConst extends UDAConst {
             {"tb_area_street"}, //34 街表
             {"tb_area_villages"}, //35 村表
             {"td_prom_courcd"}, //36 优惠券领取记录表
-            {"td_prom_rela"}, //37 优惠券领取记录表
+            {"td_prom_rela"}, //37 活动阶梯关联表
+            {"td_tran_goods"},//38 订单详情表
     };
 
 
@@ -98,6 +99,7 @@ public interface DSMConst extends UDAConst {
     public final static int TB_AREA_VILLAGES = 35;
     public final static int TB_PROM_COURCD = 36;
     public final static int TD_PROM_RELA = 37;
+    public final static int TD_TRAN_GOODS = 38;
 
 
     // TODO　切分表的规则数组 ：0 代表不需要切分（默认主从同步），1 代表按公司模型切分表,2 无需要主从同步
@@ -110,7 +112,7 @@ public interface DSMConst extends UDAConst {
             0, 0, 0, 0, 0,//20-24
             0, 0, 1, 0, 3,//25-29
             0, 3, 3, 0, 0,//30-34
-            0, 0, 0, 0, 0,
+            0, 0, 0, 1, 0,//35-39
             0, 0, 0, 0, 0,
             0, 0, 0, 0, 0,
             0, 0, 0, 0, 0,
