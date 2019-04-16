c808c17bd84063a2964f62fc4e5521a5d7282134
skyjiangwg <sky_jiangwg@163.com>
2019-04-16 21:01:40 +0800

    蒋文广 20190416 会员积分增加减少接口

--- server-user/src/main/java/com/onek/user/MemberModule.java
+++ server-user/src/main/java/com/onek/user/MemberModule.java
@@ -1,11 +1,49 @@
 package com.onek.user;
 
+import com.onek.annotation.UserPermission;
 import com.onek.context.AppContext;
 import com.onek.entitys.Result;
+import com.onek.user.entity.MemberVO;
+import constant.DSMConst;
+import dao.BaseDAO;
+import util.GsonUtils;
+
+import java.util.List;
+
+import static dao.SynDbLog.queryNative;
 
 public class MemberModule {
 
-    public Result updateMember(AppContext appContext) {
+    private static final BaseDAO baseDao = BaseDAO.getBaseDAO();
 
+    private static final String GET_SQL = "select accupoints,balpoints from {{?" + DSMConst.TD_MEMBER + "}} where compid = ?";
+    private static final String UPDATE_SQL = "update {{?" + DSMConst.TD_MEMBER + "}} set accupoints = ?, balpoints = ? where compid = ?";
+
+    @UserPermission(ignore = true)
+    public Result addPoint(AppContext appContext) {
+        String json = appContext.param.json;
+        MemberVO memberVO = GsonUtils.jsonToJavaBean(json, MemberVO.class);
+        List<Object[]> list = baseDao.queryNative(GET_SQL, new Object[]{ memberVO.getCompid()});
+        if(list != null && list.size() > 0){
+            int accupoints = Integer.parseInt(list.get(0)[0].toString());
+            int balpoints = Integer.parseInt(list.get(0)[1].toString());
+            baseDao.updateNative(UPDATE_SQL, new Object[]{ accupoints+ memberVO.getAccupoints(), balpoints + memberVO.getBalpoints(), memberVO.getCompid()});
+        }
+
+        return new Result().success(null);
+    }
+
+    @UserPermission(ignore = true)
+    public Result reducePoint(AppContext appContext) {
+        String json = appContext.param.json;
+        MemberVO memberVO = GsonUtils.jsonToJavaBean(json, MemberVO.class);
+        List<Object[]> list = baseDao.queryNative(GET_SQL, new Object[]{ memberVO.getCompid()});
+        if(list != null && list.size() > 0){
+            int accupoints = Integer.parseInt(list.get(0)[0].toString());
+            int balpoints = Integer.parseInt(list.get(0)[1].toString());
+            baseDao.updateNative(UPDATE_SQL, new Object[]{ accupoints+ memberVO.getAccupoints(), balpoints - memberVO.getBalpoints(), memberVO.getCompid()});
+        }
+
+        return new Result().success(null);
     }
 }
