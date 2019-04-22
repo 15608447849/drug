f122034be216357dc9a9ffb9ec91b8511d50b89f
ping_notebook <793065165@qq.com>
2019-04-22 15:52:01 +0800

    lzp 自动更新企业信息进入缓存

--- server-goods/src/main/java/com/onek/goods/init/GoodsInitialize.java
+++ server-goods/src/main/java/com/onek/goods/init/GoodsInitialize.java
@@ -4,14 +4,10 @@ import com.onek.goods.timer.HigherProfitsTimer;
 import com.onek.goods.timer.RemoveNewFlagTimer;
 import com.onek.server.infimp.IIceInitialize;
 import elasticsearch.ElasticSearchClientFactory;
-import elasticsearch.ElasticSearchProvider;
-
-import static Ice.Application.communicator;
 
 public class GoodsInitialize implements IIceInitialize {
     @Override
     public void startUp(String serverName) {
-        communicator().getLogger().error(serverName+" 开始初始化 --- ");
         new RemoveNewFlagTimer();
         new HigherProfitsTimer();
         ElasticSearchClientFactory.init();
---
+++ server-user/src/main/java/com/onek/user/init/UserInitialize.java
@@ -0,0 +1,84 @@
+package com.onek.user.init;
+
+import com.onek.context.StoreBasicInfo;
+import com.onek.server.infimp.IIceInitialize;
+import constant.DSMConst;
+import dao.BaseDAO;
+import redis.util.RedisUtil;
+import util.GsonUtils;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static Ice.Application.communicator;
+import static util.StringUtils.checkObjectNull;
+
+public class UserInitialize implements IIceInitialize {
+    @Override
+    public void startUp(String serverName) {
+        //启动线程更新企业信息
+        new Thread(()->{
+            while (true){
+                try {
+                    updateCompInfoToCache();
+                } catch (Exception e) {
+                    try {
+                        Thread.sleep(15  * 1000);
+                    } catch (InterruptedException ignored) {
+                    }
+                    continue;
+                }
+
+                try {
+                    Thread.sleep(15 * 60 * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+    }
+
+    private void updateCompInfoToCache() {
+        String selectSql = "SELECT cstatus,examine,cname,caddr,caddrcode,lat,lng,compid FROM {{?"+ DSMConst.D_COMP+"}} WHERE ctype=0";
+        List<Object[]> lines = BaseDAO.getBaseDAO().queryNative(selectSql);
+        if (lines == null) return;
+        for (Object[] rows : lines){
+            StoreBasicInfo info = new StoreBasicInfo();
+
+            int status = (int) rows[0];
+
+            if ((status&64) == 64){
+                status = 64; //待认证
+                info.authenticationMessage = "待认证";
+            }else if ( (status&128) == 128 ){
+                status = 128; //审核中
+                info.authenticationMessage = "审核中";
+            }else if ((status&256) == 256){
+                status = 256; //已认证
+                info.authenticationMessage = "已认证";
+            }else if ((status&512) == 512){
+                status = 512; //认证失败
+                info.authenticationMessage = "认证失败 "+checkObjectNull(rows[1],"");
+            }else if ((status&1024) == 1024){
+                status = 1024; //停用
+                info.authenticationMessage = "已停用";
+            }
+            info.authenticationStatus = status;
+            info.storeName = checkObjectNull(rows[2],"");
+            info.address = checkObjectNull(rows[3],"未设置");
+            info.addressCode = checkObjectNull(rows[4],0L);
+            info.latitude = checkObjectNull(rows[5],new BigDecimal(0)); //纬度
+            info.longitude = checkObjectNull(rows[6],new BigDecimal(0)); //精度
+            info.storeId = checkObjectNull(rows[7],0); //公司码
+            String json = GsonUtils.javaBeanToJson(info);
+            String res = RedisUtil.getStringProvide().set(""+info.storeId,json );
+            communicator().getLogger().print("更新 - Redis - 公司信息:\n" + json+" - "+res);
+
+        }
+
+
+
+
+    }
+}
