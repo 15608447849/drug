e04f174ef1fcb83a1563b6f49a341ec794daaeba
skyjiangwg <sky_jiangwg@163.com>
2019-04-24 11:54:01 +0800

    蒋文广 20190424

--- application-global/src/main/java/com/onek/util/area/AreaFeeUtil.java
+++ application-global/src/main/java/com/onek/util/area/AreaFeeUtil.java
@@ -1,4 +1,21 @@
 package com.onek.util.area;
 
+import global.IceRemoteUtil;
+import util.MathUtil;
+
 public class AreaFeeUtil {
+
+    public static double getFee(long areac){
+        double fee = 0D;
+        AreaEntity[] areaEntities = IceRemoteUtil.getAncestors(areac);
+        if(areaEntities != null && areaEntities.length > 0){
+            for(int i = areaEntities.length - 1; i>=0; i--){
+                if(areaEntities[i].getFee() > 0){
+                    fee = MathUtil.exactDiv(areaEntities[i].getFee(), 100).doubleValue();
+                    break;
+                }
+            }
+        }
+        return fee;
+    }
 }
