5d43d6a5df3a9b925c996cb399a4fdcbf4b5224b
skyjiangwg <sky_jiangwg@163.com>
2019-05-20 19:55:17 +0800

    20190520 蒋文广

--- server-discount/src/main/java/com/onek/discount/DiscountModule.java
+++ server-discount/src/main/java/com/onek/discount/DiscountModule.java
@@ -118,7 +118,7 @@ public class DiscountModule {
         if(result > 0){
             ExecutorService executors = Executors.newSingleThreadExecutor();
             executors.execute(() -> {
-                        HashMap<String,String> userMap = IceRemoteUtil.getUserByRoles(RoleCodeCons._OPER,RoleCodeCons._FINA);
+                        HashMap<String,String> userMap = IceRemoteUtil.getUserByRoles(RoleCodeCons._OPER);
                         if(userMap != null && userMap.size() > 0){
                             ProdEntity prodEntity = ProdInfoStore.getProdBySku(sku);
                             if(prodEntity != null){
