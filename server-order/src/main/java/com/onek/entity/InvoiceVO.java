fbe099d1e14f119cbb60f33dc0f05b03d3eeef3f
Helena Rubinstein <lkl9561@vip.qq.com>
2019-05-21 16:21:27 +0800

    fix bug 1644

---
+++ server-order/src/main/java/com/onek/entity/InvoiceVO.java
@@ -0,0 +1,67 @@
+package com.onek.entity;
+
+public class InvoiceVO {
+    private int oid;
+    private int cid;
+    private String taxpayer;
+    private String bankers;
+    private String account;
+    private String tel;
+    private int cstatus;
+
+    public int getOid() {
+        return oid;
+    }
+
+    public void setOid(int oid) {
+        this.oid = oid;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getTaxpayer() {
+        return taxpayer;
+    }
+
+    public void setTaxpayer(String taxpayer) {
+        this.taxpayer = taxpayer;
+    }
+
+    public String getBankers() {
+        return bankers;
+    }
+
+    public void setBankers(String bankers) {
+        this.bankers = bankers;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public int getCstatus() {
+        return cstatus;
+    }
+
+    public void setCstatus(int cstatus) {
+        this.cstatus = cstatus;
+    }
+}
