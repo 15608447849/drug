c73123c7db16faac920944bec68942cb43065bd8
ping_notebook <793065165@qq.com>
2019-05-31 16:28:44 +0800

    统一登录逻辑3

--- application-global/src/main/java/com/onek/interceptors/UserInterceptor.java
+++ application-global/src/main/java/com/onek/interceptors/UserInterceptor.java
@@ -45,38 +45,17 @@ public class UserInterceptor implements IServerInterceptor {
             * 2.注解显示不忽略拦截,拦截权限(默认不忽略)
             */
             if(up == null || !up.ignore()){
-
                 UserSession userSession = appContext.getUserSession();
-
                 if(userSession == null) {
                     return new Result().intercept(-2,"用户未登录");
                 }
-
                 if (up != null){
                     //判断是否需要认证企业
                     if(up.needAuthenticated()){
-                        if(userSession.comp == null ||  (userSession.comp.authenticationStatus & 256) <= 0){
+                        if(userSession.comp == null ||  (userSession.comp.authenticationStatus & 256) == 0){
                             return new Result().intercept(-3,"企业没有认证");
                         }
                     }
-
-                    //允许访问的角色码判断
-                    long[] roleArr = up.allowRoleList();
-                    //角色判断
-                    if (roleArr.length > 0){
-                        boolean isAccess = false;//默认不允许访问
-                        for (long role : roleArr) {
-                            if ((userSession.roleCode & role) > 0) {
-                                //当前进入的用户 有这个角色
-                                isAccess = true;
-                                break;
-                            }
-                        }
-                        if (!isAccess){
-                            return new Result().intercept(-4,"用户角色权限拒绝");
-                        }
-                    }
-
                 }
             }
 
