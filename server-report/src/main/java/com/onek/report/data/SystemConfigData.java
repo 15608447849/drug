3ec2c44484d6f95c15933c0f197814bce763d7fb
skyjiangwg <sky_jiangwg@163.com>
2019-06-04 17:51:14 +0800

    蒋文广  20190604

--- server-report/src/main/java/com/onek/report/data/SystemConfigData.java
+++ server-report/src/main/java/com/onek/report/data/SystemConfigData.java
@@ -2,6 +2,7 @@ package com.onek.report.data;
 
 import constant.DSMConst;
 import dao.BaseDAO;
+import util.StringUtils;
 
 import java.util.HashMap;
 import java.util.List;
@@ -10,6 +11,15 @@ import java.util.Map;
 import static dao.BaseDAO.getBaseDAO;
 
 public class SystemConfigData {
+
+    public static final String DAY_ACTIVE_NUM	= "DAY_ACTIVE_NUM"; //市场分析报表-日活跃数量基准数
+    public static final String WEEK_ACTIVE_NUM = "WEEK_ACTIVE_NUM"; //	市场分析报表-周活跃数量基准数
+    public static final String MONTH_ACTIVE_NUM = "MONTH_ACTIVE_NUM"; //	市场分析报表-月活跃数量基准数
+    public static final String YEAR_ACTIVE_NUM = "YEAR_ACTIVE_NUM"; //	市场分析报表-年活跃数量基准数
+    public static final String DAY_REPURCHASE_NUM	= "DAY_REPURCHASE_NUM"; //	市场分析报表-日复购数量基准数
+    public static final String WEEK_REPURCHASE_NUM = "WEEK_REPURCHASE_NUM"; //	市场分析报表-周复购数量基准数
+    public static final String MONTH_REPURCHASE_NUM = "MONTH_REPURCHASE_NUM"; //	市场分析报表-月复购数量基准数
+    public static final String YEAR_REPURCHASE_NUM = "YEAR_REPURCHASE_NUM"; //	市场分析报表-年复购数量基准数
 
     public static Map<String,String> VALS_MAP = new HashMap<>();
 
@@ -26,5 +36,13 @@ public class SystemConfigData {
 
     public static String getValByVarName(String varName){
         return VALS_MAP.get(varName);
+    }
+
+    public static Integer getIntegerValByVarName(String varName){
+        String value = VALS_MAP.get(varName);
+        if(StringUtils.isEmpty(value)){
+            return 0;
+        }
+        return Integer.parseInt(value);
     }
 }
