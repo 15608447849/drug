2fb1e9b4a927fbcb43a8168adc988c250b4f0d33
ping_notebook <793065165@qq.com>
2019-06-06 15:10:13 +0800

    登陆检查bd/bdm

--- server-user/src/main/java/com/onek/user/operations/LoginBackOp.java
+++ server-user/src/main/java/com/onek/user/operations/LoginBackOp.java
@@ -3,6 +3,13 @@ package com.onek.user.operations;
 import com.onek.context.AppContext;
 import com.onek.entitys.IOperation;
 import com.onek.entitys.Result;
+import com.onek.util.RoleCodeCons;
+import dao.BaseDAO;
+
+import java.util.List;
+
+import static constant.DSMConst.TB_COMP;
+import static constant.DSMConst.TB_SYSTEM_USER;
 
 /**
  * @Author: leeping
@@ -23,6 +30,7 @@ public class LoginBackOp extends LoginAbsOp implements IOperation<AppContext> {
             if (!checkAccountPassword(account,password)) return new Result().fail(error);
             //检测用户名/密码是否正确
             if (!checkSqlAndUserExist(context.remoteIp,account,password)) return new Result().fail(error);
+            if (!partnerCheck(account))  return new Result().fail("内部账户,拒绝登陆");
             //关联token-用户信息
             if (relationTokenUserSession(context,false)) return new Result().success("登陆成功");
         } catch (Exception e) {
@@ -30,4 +38,30 @@ public class LoginBackOp extends LoginAbsOp implements IOperation<AppContext> {
         }
         return new Result().fail("登陆失败");
     }
+
+    //判断当前手机是否是合伙人并且关联企业是否有效
+    private boolean partnerCheck(String phone){
+        String selectSql = "SELECT uid,roleid,IFNULL(cid,0) cid,belong FROM {{?" + TB_SYSTEM_USER + "}} " +
+                "WHERE cstatus&33=0 AND uphone=?";
+
+        List<Object[]> lines = BaseDAO.getBaseDAO().queryNative(selectSql,phone);
+
+        if(lines == null || lines.isEmpty()){
+            return false;
+        }
+
+        int uid = Integer.parseInt(lines.get(0)[0].toString());
+        int roleid = Integer.parseInt(lines.get(0)[1].toString());
+        if((roleid & (RoleCodeCons._PROXY_PARTNER+RoleCodeCons._DB+RoleCodeCons._DBM)) > 0){
+            int cid = Integer.parseInt(lines.get(0)[2].toString());
+            if(cid == 0){
+                return false;
+            }
+            String queryCmp = "select 1 from {{?"+TB_COMP+"}} where cid = ? and ctype = 2 and cstatus & 33 > 0 ";
+            List<Object[]> extComp = BaseDAO.getBaseDAO().queryNative(queryCmp, cid);
+            return extComp == null || extComp.isEmpty();
+        }
+        return true;
+    }
+
 }
