d310d068eb39880d9b1ecc9ef3dbe65203e69ec8
ping_notebook <793065165@qq.com>
2019-06-27 18:00:12 +0800

    ...

--- application-global/src/main/java/com/onek/mainpagebean/UiElement.java
+++ application-global/src/main/java/com/onek/mainpagebean/UiElement.java
@@ -9,7 +9,7 @@ public class UiElement {
     public String imgLink;//图片链接
     public String tempType; // 模块类型, 1.bar 2.楼层 3.专区
     public int identType;//标识类型, 如  1-满减满赠算法等...
-    public String algorithmParam; // 算法参数
+    public Object algorithmParam; // 算法参数
     public int index; //位置下标
     public  Object attr;//附加属性
 }
