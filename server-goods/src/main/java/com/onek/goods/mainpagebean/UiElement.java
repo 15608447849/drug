53a365b440a7705259989b0be674ee212477cb10
ping_notebook <793065165@qq.com>
2019-06-28 14:46:12 +0800

    ...

--- server-goods/src/main/java/com/onek/goods/mainpagebean/UiElement.java
+++ server-goods/src/main/java/com/onek/goods/mainpagebean/UiElement.java
@@ -5,11 +5,12 @@ package com.onek.goods.mainpagebean;
  * @Date: 2019/6/27 16:42
  */
 public class UiElement {
-    public String tabName;//元素页面显示名 - 如: 为你精选
-    public String imgLink;//图片链接
-    public String tempType; // 模块类型, 1.bar 2.楼层 3.专区
-    public int identType;//标识类型, 如  1-满减满赠算法等...
-    public Object algorithmParam; // 算法参数
+    public String module; // 模块类型, 1.bar 2.楼层 3.专区
+    public String name;//元素页面显示名 - 如: 为你精选
+    public int template;//模板ID
+    public long brulecode;//活动复合码
+    public String img;//图片链接
     public int index; //位置下标
     public  Object attr;//附加属性
+
 }
