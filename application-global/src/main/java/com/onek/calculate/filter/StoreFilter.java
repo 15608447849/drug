5c99085f5ebeb6b94d394b25b92c174224b85e3a
Helena Rubinstein <lkl9561@vip.qq.com>
2019-07-03 17:07:04 +0800

    upt

--- application-global/src/main/java/com/onek/calculate/filter/StoreFilter.java
+++ application-global/src/main/java/com/onek/calculate/filter/StoreFilter.java
@@ -3,6 +3,7 @@ package com.onek.calculate.filter;
 import com.onek.calculate.entity.IDiscount;
 import com.onek.calculate.entity.IProduct;
 import com.onek.util.stock.RedisStockUtil;
+import org.hyrdpf.util.LogUtil;
 
 /**
  * 库存过滤器
@@ -12,6 +13,9 @@ public class StoreFilter extends BaseFilter {
     @Override
     protected boolean isFilter(IDiscount iDiscount, IProduct product) {
         int store = RedisStockUtil.getActStockBySkuAndActno(product.getSKU(), iDiscount.getDiscountNo());
+
+        LogUtil.getDefaultLogger().info("Current actStore is " + store
+                + ", sku is " + product.getSKU() + " AND actNo is " + iDiscount.getDiscountNo());
 
         return store <= 0;
     }
