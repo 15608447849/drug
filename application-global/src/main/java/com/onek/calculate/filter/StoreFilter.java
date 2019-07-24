90ac5d76a2669bc7a8fecf23e2f025038c0a6390
Helena Rubinstein <lkl9561@vip.qq.com>
2019-07-24 10:03:46 +0800

    upt

--- application-global/src/main/java/com/onek/calculate/filter/StoreFilter.java
+++ application-global/src/main/java/com/onek/calculate/filter/StoreFilter.java
@@ -2,6 +2,8 @@ package com.onek.calculate.filter;
 
 import com.onek.calculate.entity.IDiscount;
 import com.onek.calculate.entity.IProduct;
+import com.onek.calculate.entity.Package;
+import com.onek.calculate.entity.Product;
 import com.onek.util.stock.RedisStockUtil;
 import org.hyrdpf.util.LogUtil;
 
@@ -12,11 +14,36 @@ public class StoreFilter extends BaseFilter {
 
     @Override
     protected boolean isFilter(IDiscount iDiscount, IProduct product) {
-        int store = RedisStockUtil.getActStockBySkuAndActno(product.getSKU(), iDiscount.getDiscountNo());
+        long discountNo = iDiscount.getDiscountNo();
 
-        LogUtil.getDefaultLogger().info("Current actStore is " + store
-                + ", sku is " + product.getSKU() + " AND actNo is " + iDiscount.getDiscountNo());
+        if (product instanceof Package) {
+            Package p = (Package) product;
 
-        return store <= 0;
+            if (p.getExpireFlag() == 0) {
+                int store;
+                for (Product product1 : p.getPacageProdList()) {
+                    store = RedisStockUtil.getActStockBySkuAndActno(product1.getSKU(), discountNo);
+
+                    if (store < product1.getNums()) {
+                        LogUtil.getDefaultLogger().info("This actStore is Not Enough, store is " + store
+                                + ", sku is " + product1.getSKU() + " AND actNo is " + discountNo);
+
+                        return true;
+                    }
+                }
+            }
+
+        } else {
+            int store = RedisStockUtil.getActStockBySkuAndActno(product.getSKU(), discountNo);
+
+            if (store <= 0) {
+                LogUtil.getDefaultLogger().info("This actStore is Empty"
+                        + ", sku is " + product.getSKU() + " AND actNo is " + discountNo);
+
+                return true;
+            }
+        }
+
+        return false;
     }
 }
