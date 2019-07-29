23eb64f64e78dc11434009685352de355e725545
Helena Rubinstein <>
2019-07-29 22:25:40 +0800

    u

--- server-order/src/main/java/com/onek/order/CouponRevModule.java
+++ server-order/src/main/java/com/onek/order/CouponRevModule.java
@@ -619,6 +619,7 @@ public class CouponRevModule {
         resultMap.put("acpay",payamt);
         resultMap.put("payamt",payamt);
         resultMap.put("payflag",0);
+        resultMap.put("rebeatp", calculate.getRebeatValue());
         if(couponUseDTOS.get(0).getBalway() > 0 && bal > 0){
             resultMap.put("bal",bal);
             if(bal >= payamt){
