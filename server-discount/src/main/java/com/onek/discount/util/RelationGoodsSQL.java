1696ca650e75dc8e60407a36b04d74a8ba95540d
Helena Rubinstein <>
2019-07-29 20:16:44 +0800

    u

--- server-discount/src/main/java/com/onek/discount/util/RelationGoodsSQL.java
+++ server-discount/src/main/java/com/onek/discount/util/RelationGoodsSQL.java
@@ -16,22 +16,25 @@ public class RelationGoodsSQL {
             " SUBSTR(sku,2,2) as classno2,sku,systore, sum(used) as uused, (systore-sum(used)) as notused from ( " +
             " select sku,systore, used  from ( " +
             " SELECT sku,gcode,(store-freezestore) as systore, " +
-            " ceil(IF( ua.cstatus & 256 > 0 and actstock <100, sum( actstock ), 0 ) * 0.01 * ( store - freezestore ) + IF " +
+            " ceil(IF( ua.cstatus & 256 > 0 and actstock <100 and actstock>0, sum( actstock ), 0 ) * 0.01 * ( store - freezestore ) + IF " +
             " ( ua.cstatus & 256 = 0, sum( actstock ), 0 ) ) AS used FROM  " +
             " (SELECT " +
             " spu,sku,gcode,store,freezestore,actstock,a.cstatus  " +
             " FROM {{?" + DSMConst.TD_PROM_ASSDRUG + "}} a " +
             " LEFT JOIN td_prod_sku s ON s.sku LIKE CONCAT( '_', a.gcode, '%' )  " +
             " WHERE a.cstatus & 1 = 0 AND length( gcode ) < 14 AND gcode > 0  " +
-            " AND actcode IN ( SELECT unqid FROM td_prom_act WHERE cstatus & 1 = 0 )  and actcode<>? UNION ALL " +
+            " AND actcode IN ( SELECT unqid FROM td_prom_act WHERE cstatus & 1 = 0 "
+            + " and  cstatus&2048>0 and ckstatus=2)  and actcode<>? UNION ALL " +
             " SELECT spu, sku,gcode, store, freezestore,actstock, a.cstatus  " +
             " FROM td_prom_assdrug a LEFT JOIN td_prod_sku s ON s.sku = gcode  " +
             " WHERE a.cstatus & 1 = 0  AND length( gcode ) = 14  " +
-            " AND actcode IN ( SELECT unqid FROM td_prom_act WHERE cstatus & 1 = 0 )  and actcode<>? UNION ALL " +
+            " AND actcode IN ( SELECT unqid FROM td_prom_act WHERE cstatus & 1 = 0 "
+            + "  and  cstatus&2048>0 and ckstatus=2)  and actcode<>? UNION ALL " +
             " SELECT spu, sku, gcode, store, freezestore, actstock, a.cstatus  " +
             " FROM td_prom_assdrug a, td_prod_sku s  " +
             " WHERE a.cstatus & 1 = 0  AND gcode = 0  " +
-            " AND actcode IN ( SELECT unqid FROM td_prom_act WHERE cstatus & 1 = 0 )  and actcode<>? " +
+            " AND actcode IN ( SELECT unqid FROM td_prom_act WHERE cstatus & 1 = 0 "
+            + "  and  cstatus&2048>0 and ckstatus=2)  and actcode<>? " +
             " ) ua " +
             " WHERE ua.cstatus & 1 = 0  " +
             " GROUP BY sku , ua.cstatus ) ub UNION ALL  " +
