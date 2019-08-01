ab374c542e05c6380fb9bbdc0d47e8f34900ec04
Helena Rubinstein <>
2019-08-01 17:54:17 +0800

    u

--- publib/src/main/java/util/StringUtils.java
+++ publib/src/main/java/util/StringUtils.java
@@ -13,7 +13,7 @@ import java.util.regex.Pattern;
 public class StringUtils {
     private static final String INTEGER_REGEX = "0|-?([1-9]{1}[0-9]*)";
     private final static String EMAIL_REGEXP =
-            "^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$";
+            "^[\\.a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$";
 
     //字符串不为空
     public static boolean isEmpty(String str){
