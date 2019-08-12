b0246d4055881ee9edbcd60752e2e60462c9d19d
liuhuics <liuhuics@126.com>
2019-08-12 15:55:27 +0800

    BD指派

--- server-user/src/main/java/com/onek/user/BackGroundProxyMoudule.java
+++ server-user/src/main/java/com/onek/user/BackGroundProxyMoudule.java
@@ -1010,8 +1010,8 @@ public class BackGroundProxyMoudule {
 
         if((roleCode & (RoleCodeCons._PROXY_PARTNER +RoleCodeCons._PROXY_MGR)) > 0
                 && (roleid & RoleCodeCons._DBM) > 0){
-            String selectSQL = " select uid,urealname from {{?" + DSMConst.TB_SYSTEM_USER + "}} where roleid & ? > 0 and cstatus & 1 = 0 and cid = ? " +
-                    "and uid in (select distinct uid from {{?" +DSMConst.TB_PROXY_UAREA+"}} where areac = ? and cstatus & 1 = 0)";
+            String selectSQL = " select uid,urealname from {{?" + DSMConst.TB_SYSTEM_USER + "}} where roleid & ? > 0 and cstatus & 1 = 0 and cid = ? ";
+                   // "and uid in (select distinct uid from {{?" +DSMConst.TB_PROXY_UAREA+"}} where areac = ? and cstatus & 1 = 0)";
             queryResult = baseDao.queryNative(selectSQL,roleid,cid,areac);
             if (queryResult == null || queryResult.isEmpty()) return result.success(null);
             return  result.success(convBdList(queryResult));
@@ -1019,8 +1019,9 @@ public class BackGroundProxyMoudule {
 
         if((roleCode & RoleCodeCons._DBM) > 0 &&  (roleid & RoleCodeCons._DB) > 0){
             String selectSQL =  " select uid,urealname from {{?" + DSMConst.TB_SYSTEM_USER + "}} where roleid & ? > 0" +
-                    " and cstatus & 1 = 0 and belong = ? or uid = ? "+
-            "and uid in (select distinct uid from {{?" +DSMConst.TB_PROXY_UAREA+"}} where areac = ? and cstatus & 1 = 0)";
+                    " and cstatus & 1 = 0 and belong = ? or uid = ? ";
+                   // +
+           // "and uid in (select distinct uid from {{?" +DSMConst.TB_PROXY_UAREA+"}} where areac = ? and cstatus & 1 = 0)";
             queryResult = baseDao.queryNative(selectSQL,roleid,uid,uid,areac);
             if (queryResult == null || queryResult.isEmpty()) return result.success(null);
             return  result.success(convBdList(queryResult));
