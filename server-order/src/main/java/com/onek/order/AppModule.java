209071946100a774f9bee3637b9c7a30aefd4172
zhouliao <2039958472@qq.com>
2019-08-13 16:16:19 +0800

    update

--- server-order/src/main/java/com/onek/order/AppModule.java
+++ server-order/src/main/java/com/onek/order/AppModule.java
@@ -64,6 +64,9 @@ public class AppModule {
             }else{//手动输入
                 beanGop.number = beanGop.number - pkgnum;
             }
+            if(beanGop.number <= 0 ){
+                return new Result().success("加入购物车成功","调用成功");
+            }
             context.logger.print("最终添加购物车套餐数量 = " + beanGop.number);
             Result res =  writePkgToShopCat(compid,beanGop.pkgno,beanGop.number,context);
 
