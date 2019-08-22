21018e5cb8aebb8e221e79ac8eb8d4dde9ae6ab4
ping_notebook <793065165@qq.com>
2019-08-22 16:31:26 +0800

    多用户关联

--- server-user/src/main/java/com/onek/user/LoginRegistrationModule.java
+++ server-user/src/main/java/com/onek/user/LoginRegistrationModule.java
@@ -302,7 +302,9 @@ public class LoginRegistrationModule {
         String sql3 = "SELECT u.uid,u.cid,u.uphone,u.upw,c.cname,c.caddrcode,c.caddr FROM {{?" +TB_SYSTEM_USER+"}} AS u INNER JOIN {{?"+TB_COMP+"}} AS c ON u.cid=c.cid WHERE u.uid IN("+sql2+")";
         List<Object[]> lines = BaseDAO.getBaseDAO().queryNative(sql3,uid);
         List<RelationBean> list = new ArrayList<>();
+
         if (lines.size() > 0){
+            RelationBean current = null;
             for (Object[] rows : lines){
                 RelationBean b  = new RelationBean();
                 b.uid = StringUtils.obj2Str(rows[0]);
@@ -316,12 +318,23 @@ public class LoginRegistrationModule {
                 try{
                     b.caddrcodeStr = IceRemoteUtil.getArean(Long.parseLong(b.caddrcode));
                     b.isCurrent = b.uid.equals(uid);
+                    current = b;
                 }catch (Exception ignored){ }
 
-                list.add(b);
+                if (!b.isCurrent) list.add(b);
             }
+            if (current!=null) list.add(0,current);
         }
         return new Result().success(list);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i<5;i++){
+            list.add(i);
+        }
+        list.add(0,9);
+        System.out.println(list);
     }
 
     public Result tryRelationUser(AppContext appContext){
