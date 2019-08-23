979eae194380700568a58ff6b2d67b984a8b6d9a
skyjiangwg <sky_jiangwg@163.com>
2019-08-23 10:46:37 +0800

    还原代码 jiangwenguang 20190823

--- server-user/src/main/java/com/onek/user/init/UserInitialize.java
+++ server-user/src/main/java/com/onek/user/init/UserInitialize.java
@@ -8,26 +8,26 @@ public class UserInitialize implements IIceInitialize {
     @Override
     public void startUp(String serverName) {
 
-        //启动线程更新企业信息
-        new Thread(()->{
-//            while (true){
-                try {
-                    try {
-                        Thread.sleep(30 * 1000L);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    updateCompInfoToCache();
-                    try {
-                        Thread.sleep( 12 * 60 * 60 * 1000L);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-//            }
-        }).start();
+//        //启动线程更新企业信息
+//        new Thread(()->{
+////            while (true){
+//                try {
+//                    try {
+//                        Thread.sleep(30 * 1000L);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    updateCompInfoToCache();
+//                    try {
+//                        Thread.sleep( 12 * 60 * 60 * 1000L);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+////            }
+//        }).start();
 
     }
 
