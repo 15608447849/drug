55cbb8a1ac8dd7d680ed1974b96102e703922316
zhouliao <2039958472@qq.com>
2019-09-05 14:40:11 +0800

    update

--- server-user/src/main/java/com/onek/user/operations/BDAchievementOP.java
+++ server-user/src/main/java/com/onek/user/operations/BDAchievementOP.java
@@ -27,7 +27,7 @@ public class BDAchievementOP {
     private static final BaseDAO baseDao = BaseDAO.getBaseDAO();
 
     /*查询地区*/
-    private static String _QUERY_AREA_USER = "select uid,areac from {{?"+DSMConst.TB_PROXY_UAREA+"}} where areac=? GROUP BY uid";
+    private static String _QUERY_AREA_USER = "select uid,areac from {{?"+DSMConst.TB_PROXY_UAREA+"}} where areac=? and cstatus&1 = 0 and cstatus&128>0 GROUP BY uid";
 
     private static String _QUERY_USER_BELONG = "SELECT uid from {{?"+DSMConst.TB_SYSTEM_USER+"}} WHERE belong = ?";
 
