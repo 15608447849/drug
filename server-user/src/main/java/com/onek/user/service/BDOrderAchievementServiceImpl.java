ceefb88c02ead02b556a837c5a504cd15a4d09a2
zhouliao <2039958472@qq.com>
2019-09-06 18:31:28 +0800

    update

--- server-user/src/main/java/com/onek/user/service/BDOrderAchievementServiceImpl.java
+++ server-user/src/main/java/com/onek/user/service/BDOrderAchievementServiceImpl.java
@@ -41,12 +41,20 @@ public class BDOrderAchievementServiceImpl {
         if(bdsum.isEmpty()){
             jsonObject.put("cumulticeSum",0); //累计首购数
         }else{
-            jsonObject.put("cumulticeSum",bdsum.get(uid).toString().isEmpty() ? "0" : bdsum.get(uid).toString()); //累计首购数
+            if(bdsum.get(uid)==null){
+                jsonObject.put("cumulticeSum",0); //累计首购数
+            }else {
+                jsonObject.put("cumulticeSum", bdsum.get(uid).toString().isEmpty() ? "0" : bdsum.get(uid).toString()); //累计首购数
+            }
         }
         if(bdNewAddSum.isEmpty()){
             jsonObject.put("cumulticeNewAdd", 0);//新增首购数
         }else {
-            jsonObject.put("cumulticeNewAdd", bdNewAddSum.get(uid).toString().isEmpty() ? "0" : bdNewAddSum.get(uid).toString());//新增首购数
+            if(bdNewAddSum.get(uid)==null){
+                jsonObject.put("cumulticeNewAdd", 0);//新增首购数
+            }else {
+                jsonObject.put("cumulticeNewAdd",bdNewAddSum.get(uid).toString().isEmpty() ? "0" : bdNewAddSum.get(uid).toString());//新增首购数
+            }
         }
         BDToOrderAchieveemntVO order = getOrderInfo(oList,uid);
         if(order == null) {
