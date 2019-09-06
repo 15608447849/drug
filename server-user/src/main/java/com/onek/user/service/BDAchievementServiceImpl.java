1f58f76a3d87d93bc1709ae35d4cd8bb97532592
zhouliao <2039958472@qq.com>
2019-09-06 17:44:50 +0800

    update

--- server-user/src/main/java/com/onek/user/service/BDAchievementServiceImpl.java
+++ server-user/src/main/java/com/onek/user/service/BDAchievementServiceImpl.java
@@ -280,14 +280,14 @@ public class BDAchievementServiceImpl {
 //								bdjson2.put("roleid", csjlrole);
 //								bdm.put(bdjson2);
                                 //if(!csUids.contains(csjluid)) {
-                                BDAchievementVO bd2 = new BDAchievementVO();
-                                bd2.setCid(0);
-                                bd2.setUid(csjluid);
-                                bd2.setRoleid(csjlrole);
-                                bd2.setUrealname(csjlname);
-                                bd2.setBelong(csjluid);
-                                bd2.setFlag(3);
-                                bdms.add(bd2);
+//                                BDAchievementVO bd2 = new BDAchievementVO();
+//                                bd2.setCid(0);
+//                                bd2.setUid(csjluid);
+//                                bd2.setRoleid(csjlrole);
+//                                bd2.setUrealname(csjlname);
+//                                bd2.setBelong(csjluid);
+//                                bd2.setFlag(3);
+//                                bdms.add(bd2);
                                // }
                                 //csjluid =
                             }
